f5db6a9053ba368a35915d61c69a23e0ff3fda31
Kaiqi Liang <kaiqi.liang@outlook.com>
2023-05-06 15:13:18 +0800

    Add clang format

--- src/master_pkg/src/master.cpp
+++ src/master_pkg/src/master.cpp
@@ -4,23 +4,20 @@
 bool manual = false;
 ros::Publisher cmd_vel_pub;
 
-void joy_callback(const sensor_msgs::Joy::ConstPtr& joy_msg)
-{
+void joy_callback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
 	if (joy_msg->buttons[1]) {
-    	manual = true;
+		manual = true;
 	} else if (joy_msg->buttons[2]) { // Enable automated mode
-    	manual = false;
+		manual = false;
 	}
-  if (manual) {
-    cmd_vel_pub.publish(joy_msg);
-  }
+	if (manual) cmd_vel_pub.publish(joy_msg);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 	ros::init(argc, argv, "master");
-  ros::NodeHandle n;
+	ros::NodeHandle n;
 	ros::Subscriber sub = n.subscribe("joy", 1000, joy_callback);
-  cmd_vel_pub = n.advertise<sensor_msgs::Joy>("master/joy", 1);
+	cmd_vel_pub = n.advertise<sensor_msgs::Joy>("master/joy", 1);
 	ros::spin();
 	return 0;
 }
