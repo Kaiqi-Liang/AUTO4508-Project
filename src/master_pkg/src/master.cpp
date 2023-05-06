9a9756064ee364908aaf87b6b5f790f839eeb1d0
Pioneer <kaiqi.liang@outlook.com>
2023-05-06 18:04:32 +0800

    Connect gps

--- src/master_pkg/src/master.cpp
+++ src/master_pkg/src/master.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
+#include "sensor_msgs/NavSatFix.h"
 
 bool manual = false;
 ros::Publisher cmd_vel_pub;
@@ -13,10 +14,15 @@ void joy_callback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
 	if (manual) cmd_vel_pub.publish(joy_msg);
 }
 
+void gps_callback(const sensor_msgs::NavSatFix::ConstPtr& gps_fix_msg) {
+	ROS_INFO("%f %f", gps_fix_msg->latitude, gps_fix_msg->longitude);
+}
+
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "master");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("joy", 1000, joy_callback);
+	ros::Subscriber joy_sub = n.subscribe("joy", 1000, joy_callback);
+	ros::Subscriber gps_sub = n.subscribe("fix", 1000, gps_callback);
 	cmd_vel_pub = n.advertise<sensor_msgs::Joy>("master/joy", 1);
 	ros::spin();
 	return 0;
