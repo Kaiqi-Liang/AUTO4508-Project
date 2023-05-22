e64633a7789b82960e01be87839877e5a4f1f6d8
Pioneer Robot <kaiqi.liang@outlook.com>
2023-05-22 14:01:39 +0800

    Plot map on rviz

--- src/master_pkg/scripts/plot.py
+++ src/master_pkg/scripts/plot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,16 +7,19 @@ import cartopy.crs as ccrs
 from io import BytesIO
 import cv2
 import rospy
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import NavSatFix
 
 def gps_callback(data):
-    df = pd.read_csv('coordinate.csv', header=None)
+    df = pd.read_csv('../AUTO4508-Project/src/master_pkg/src/coordinate.csv', header=None)
     latitude = df.iloc[:,0].tolist()
     longitude = df.iloc[:,1].tolist()
 
-    latitude.append(data.latitude)
-    longitude.append(data.longitude)
+    if not math.isnan(data.latitude):
+        latitude.append(data.latitude)
+    if not math.isnan(data.longitude):
+        longitude.append(data.longitude)
 
     # Create a map projection
     projection = ccrs.PlateCarree()
@@ -27,17 +31,18 @@ def gps_callback(data):
     ax.plot(longitude, latitude, 'bo', transform=projection)
     ax.set_title('GPS Coordinates Plot')
 
-    # Show the plot
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
+    fig = plt.gcf()
+    fig.canvas.draw()
+    img = np.array(fig.canvas.renderer._renderer)
+    plt.close(1)
 
-    img = np.asarray(bytearray(buffer.read()), dtype=np.uint8)
-    data.data = img.tobytes()
+    bridge = CvBridge()
+    image_msg = bridge.cv2_to_imgmsg(img, encoding="rgba8")
+
     pub = rospy.Publisher('plot_gps', Image, queue_size=10)
-    pub.publish(data)
+    pub.publish(image_msg)
 
 if __name__ == '__main__':
     rospy.init_node('plot', anonymous=True)
-    rospy.Subscriber('gps', NavSatFix, gps_callback)
+    rospy.Subscriber('fix', NavSatFix, gps_callback)
     rospy.spin()
